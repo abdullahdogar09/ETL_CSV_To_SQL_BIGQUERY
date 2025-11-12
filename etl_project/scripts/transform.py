cba99556610d6b0f030805f5eb95226d7e597eb7
abdullahdogar09 <abdullah009dogar@gmail.com>
2025-11-12 12:07:00 +0500

    Created transformation File

---
+++ etl_project/scripts/transform.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+def transform_data(df):
+    try:
+        # Change column names to uppercase
+        df.columns = df.columns.str.upper(inplace=True)
+        #-----------------------------------------------------------------------------------
+        # Create an index column
+        df.reset_index(inplace=True)
+        #----------------------------------------------------------------------------------- 
+        #create a labelling column 
+        conditions = [
+            df["COMPANY_SIZE"].between(1,3),
+            df["COMPANY_SIZE"].between(4,6),
+            df["COMPANY_SIZE"] >= 7
+        ]
+        labels = ["micro-company", "mini-company", "small-company"]
+        df["COMPANY_SIZE_LABELS"] = np.select(conditions, labels, default="unknown")
+        #-----------------------------------------------------------------------------------
+        # Drop the last row in the dataframe
+        df.drop(df.tail(1).index, inplace=True)
+        #-----------------------------------------------------------------------------------
+        # filter out rows where state = 0
+        df =df[df["STATE"] != 0]
+        #-----------------------------------------------------------------------------------
+        # sort the df by ascending order of company_id
+        df = df.sort_values(by="COMPANY_ID", ascending=True)
+        #-----------------------------------------------------------------------------------
+        # drop duplicate rows based on company_id, keeping the first occurance
+        df.drop_duplicates(subset="COMPANY_ID", keep="first", inplace=True)
+        #-----------------------------------------------------------------------------------
+
+        print("Transformation complete.")
+        return df
+    except Exception as e:
+        print(f"Error transforming file: {e}")
+        return df
